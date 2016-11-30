766b120492ff3274953884f54a82b1dd718308f4
laurensmalld <lauren.dentremont@gmail.com>
2016-11-30 19:02:42 +0000

    made it prettier

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,11 @@
 class GenresController < ApplicationController
   def show
-    @fiction_ns = GoogleBooks.search('"Nova Scotia", categories: fiction',
+    @fiction_ns = GoogleBooks.search('"Nova Scotia", subject: fiction',
                 {:api_key => "AIzaSyCvKlUy4ZaVE_QxdMVZ7T37rTyEJZI-9Wc",
-                  :count => 20,
-                  :order_by => 'newest'})
+                  :count => 10})
+    @nonfiction_ns = GoogleBooks.search('"Nova Scotia", subject: fiction',
+                {:api_key => "AIzaSyCvKlUy4ZaVE_QxdMVZ7T37rTyEJZI-9Wc",
+                  :count => 10})
 
   end
 end

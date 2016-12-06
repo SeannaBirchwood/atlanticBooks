02d4bf6b332c1521f57abcf7fbf1194ceeba5ae8
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-12-06 17:10:48 +0000

    default user profile picture is now showing up in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,22 +6,25 @@ class UsersController < ApplicationController
 
   def index
     @user = User.all
+    @user.profilePic="../public/default-profile.png" if @user.avatar.empty?
     #order by how many books own/read?
   end
 
   def show
     @user = User.find(params[:id])
+    @user.profilePic="../public/default-profile.png" if @user.avatar.empty?
   end
 
-  def profile
-    @user = User.find params[:id]
-    #@review = Review.new
-    #@reviews = @book.reviews
-  end
+  # def profile
+  #   @user = User.find params[:id]
+  #   #@review = Review.new
+  #   #@reviews = @book.reviews
+  # end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    @user.profilePic = "../../public/default-profile.png"
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/login'
     else
